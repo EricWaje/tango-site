231451b1d1d0c0cd17978fcea6cd7e0886f653de
Eric Wajnrajch <ericwaje03@gmail.com>
2021-09-28 23:00:39 -0300

    agregando EmailJs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 const menu = document.getElementById('menu');
 const hamb = document.getElementById('hamb');
+//FORM
+const btn = document.getElementById('submit');
+const form = document.getElementById('formulario');
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const comentarios = document.getElementById('textArea');
 
 const items = document.getElementsByClassName('item');
 for (let i = 0; i < items.length; i++) {
@@ -22,7 +28,51 @@ const mostrarMenu = () => {
     }
 };
 
+(function () {
+    emailjs.init('user_hCqWm2f1dBjU8MxczFLo6');
+})();
+
+const sendEmail = (e) => {
+    e.preventDefault();
+    validarCampos();
+};
+
+const enviarForm = (nombre, email, comentarios) => {
+    emailjs.send('service_lgkw6fh', 'template_8ul9lqf', {
+        nombre: nombre,
+        email: email,
+        comentarios: comentarios,
+    });
+};
+
+const validarCampos = () => {
+    if (nombre.value === '' || email.value === '' || comentarios.value === '') {
+        completarCampos();
+    } else {
+        enviarForm(nombre.value, email.value, comentarios.value);
+        exito();
+        form.reset();
+    }
+};
+
+const completarCampos = () => {
+    swal({
+        text: 'Todos los campos son requeridos',
+        icon: 'warning',
+        button: 'Cerrar',
+    });
+};
+
+const exito = () => {
+    swal({
+        text: 'Formulario enviado con éxito!',
+        icon: 'success',
+        button: 'Cerrar',
+    });
+};
+
 const start = () => {
+    btn.addEventListener('click', sendEmail);
     hamb.addEventListener('click', mostrarMenu);
 };
 
